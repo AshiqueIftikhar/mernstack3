25e4008d4c0d9abdf50ee3cbae35ead6eb7e79d9
Ashique Iftikhar <ashique.iftikhar@gmail.com>
2023-03-22 12:57:49 +0600

    initial commit

---
+++ multiplicationTable.js
@@ -0,0 +1,25 @@
+
+function multiplicationTable(num)
+{
+    for(var i =1; i<=10; i++)
+    {
+      //num x iteration = product
+      console.log(`${num} X ${i} = ${(parseInt(num)*i)}`);
+      
+    }
+}
+
+//test 1
+multiplicationTable(2);
+
+//test 2 
+multiplicationTable(3);
+
+//test 3
+multiplicationTable(5);
+
+//test 4
+multiplicationTable(7);
+
+//test 10
+multiplicationTable(10);
