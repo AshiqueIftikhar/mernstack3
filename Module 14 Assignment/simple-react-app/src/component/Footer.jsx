73560caeb4f4546fc9d69eaa9ad133a0aa67062d
Ashique Iftikhar <ashique.iftikhar@gmail.com>
2023-07-14 20:11:02 +0600

    quote added in footer

--- Module 14 Assignment/simple-react-app/src/component/Footer.jsx
+++ Module 14 Assignment/simple-react-app/src/component/Footer.jsx
@@ -1,6 +1,8 @@
 const Footer = () => {
     return (
         <div>
+            <blockquote>“Any fool can write code that a computer can understand. Good programmers write code that humans can understand.”
+― Martin Fowler</blockquote>
             <h5>&copy; Ashique Iftikhar 2023</h5>
         </div>
     );
